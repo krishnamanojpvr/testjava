49d8b5dcad3f14fce5b1d4d230303637cbf43246
krishnamanojpvr <pvrkmsbunny@gmail.com>
2024-12-29 16:45:08 +0530

    logo

---
+++ src/main/java/maven_webproj/testjava/App.java
@@ -0,0 +1,13 @@
+package maven_webproj.testjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello test java!" );
+    }
+}
